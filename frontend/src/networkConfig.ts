7342f257a2bf3b022f822936842394c0bd3ff7dc
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-12-13 11:48:27 +0530

    fix network config file

--- frontend/src/networkConfig.ts
+++ frontend/src/networkConfig.ts
@@ -9,9 +9,9 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        packageId: '0xf768ffc7553f2971ffec6e619976aa5979874fceba437fe8ba21b88e49184d3c',
-        treasuryCap: '0x6d420a53d6b028bc228aabafb2abdccf464507ce66d7184138340cfe946b624d',
-        coinAddress: '0x28f1aef1424da9817a55de3184b8a960e5eaf5968e185a8d318876bf3c8e7de9'
+        packageId: '<Your_Package_Address>',
+        treasuryCap: '<Your_TreasuryCap_Address>',
+        coinAddress: '<Your_Token_Address>'
       },
     },
     mainnet: {
