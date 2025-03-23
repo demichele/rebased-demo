b67802f55f4371c6ba48c9bc74307da476eed429
Daniel De Michele <carpdemichele@gmail.com>
2025-03-23 22:06:11 +0100

    Transfer demo

--- frontend/src/networkConfig.ts
+++ frontend/src/networkConfig.ts
@@ -9,9 +9,9 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        packageId: '<YOUR_PACKAGE_ADDRESS>',
-        treasuryCap: '<YOUR_TREASURYCAP_ADDRESS>',
-        coinAddress: '<YOUR_TOKEN_ADDRESS>'
+        packageId: '0xa8dab537682c58818c057e3db8caab4eabe80a53a90649f0b6af880c0e15d1b8',
+        treasuryCap: '0x12f7cebe7edd0eb28ff96a8ef414b0c3bdc79cd71192393e2af7b310905e9ca1',
+        coinAddress: '0x1e489a66215cd2fc0fea7ed3801fdf1eed7375ed3eefbb44e12abaab2391ce10'
       },
     },
     mainnet: {
